8dc1faa65939dd21407f7482decd837e8850c4b1
Vermasushant144 <Vermasushant144@gmail.com>
2025-10-17 23:34:35 +0530

    Update Navbar.tsx
    admin

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -57,7 +57,7 @@ export const Navbar = () => {
             <Link to="/admin" onClick={() => setOpen(false)}>
               <Button variant="ghost" className="w-full justify-start">
                 <LayoutDashboard className="w-5 h-5 mr-2" />
-                Admin Dashboard
+                Admin Dashboards
               </Button>
             </Link>
           )}

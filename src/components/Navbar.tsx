602e2aac750a62390967823c9a97f8d76e9871f2
Vermasushant144 <Vermasushant144@gmail.com>
2025-10-18 15:49:32 +0530

    Update Navbar.tsx
    updated

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -71,7 +71,7 @@ export const Navbar = () => {
         <>
           <Link to="/login" onClick={() => setOpen(false)}>
             <Button variant="ghost" className="font-medium w-full justify-start">
-              LogIn
+              Login
             </Button>
           </Link>
           <Link to="/signup" onClick={() => setOpen(false)}>
